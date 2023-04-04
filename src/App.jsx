f2d664c7add1eecc1c5cbcd9d66fc02a2c76d7f7
vsompura3 <vsompura3@gmail.com>
2023-04-05 01:03:07 +0530

    Initial project setup with react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <main className="text-center space-y-6 sm:space-y-12 py-4">
+      <h1 className="text-2xl sm:text-5xl font-bold">
+        Frontend Mentor Project Setup
+      </h1>
+      <footer>
+        <div class="attribution">
+          Challenge by{' '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{' '}
+          <a
+            className="underline hover:text-teal-500"
+            class
+            href="https://github.com/vsompura3"
+            rel="noreferrer noopener"
+          >
+            Vaibhav Sompura
+          </a>
+          .
+        </div>
+      </footer>
+    </main>
+  )
+}
